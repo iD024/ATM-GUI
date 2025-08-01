eae0acd6f1102cd7f0de39cab9d9c825e3fd50f5
Lakshya SIngh <iD024>
2025-08-01 14:04:38 +0530

    Removed GTK and added RayGui

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
-#include <gtk/gtk4> 
+#include "raylib.h"
 
-int main() {
-    
+#define RAYGUI_IMPLEMENTATION
+#include "/home/mrid/Documents/C&C++/ATM-GUI/raygui.h"
+
+int main(void)
+{
+    InitWindow(800, 450, "raygui Slider Example");
+
+    float value = 50.0f;
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        GuiSliderBar((Rectangle){ 300, 200, 200, 20 },
+                     "Min", "Max", &value, 0, 100);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
-
-
